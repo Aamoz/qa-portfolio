817a968342be02c8977938e8825c345a176766f6
Aamoz <148727365+Aamoz@users.noreply.github.com>
2026-06-11 19:29:43 +0100

    Fix headless Chrome for CI

--- selenium-tests/test_checkboxes.py
+++ selenium-tests/test_checkboxes.py
@@ -1,30 +1,27 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
 
 def test_checkbox_checked(driver):
     driver.get("https://the-internet.herokuapp.com/checkboxes")
     checkboxes = driver.find_elements(By.CSS_SELECTOR, "input[type='checkbox']")
-    
-    checkbox1 = checkboxes[0]
-    checkbox2 = checkboxes[1]
-    
-    assert not checkbox1.is_selected()
-    assert checkbox2.is_selected()
+    assert not checkboxes[0].is_selected()
+    assert checkboxes[1].is_selected()
 
 def test_checkbox_can_be_checked(driver):
     driver.get("https://the-internet.herokuapp.com/checkboxes")
     checkboxes = driver.find_elements(By.CSS_SELECTOR, "input[type='checkbox']")
-    
     checkbox1 = checkboxes[0]
-    
     checkbox1.click()
-    
     assert checkbox1.is_selected()
