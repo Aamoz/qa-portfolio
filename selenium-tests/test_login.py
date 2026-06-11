ffde3d1e060a60a5f528d08380c2f100280593f8
Aamoz <148727365+Aamoz@users.noreply.github.com>
2026-06-11 19:29:05 +0100

    Fix headless Chrome for CI

--- selenium-tests/test_login.py
+++ selenium-tests/test_login.py
@@ -3,10 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
 
