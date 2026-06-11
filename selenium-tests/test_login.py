d86b6bb9b90e555fba3473a110df67dbe1224a82
Aamoz <148727365+Aamoz@users.noreply.github.com>
2026-06-11 17:32:19 +0100

    Add Selenium test suite

---
+++ selenium-tests/test_login.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+def test_successful_login(driver):
+    driver.get("https://the-internet.herokuapp.com/login")
+    driver.find_element(By.ID, "username").send_keys("tomsmith")
+    driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+    message = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, ".flash.success"))
+    )
+    assert "You logged into a secure area!" in message.text
+
+def test_failed_login(driver):
+    driver.get("https://the-internet.herokuapp.com/login")
+    driver.find_element(By.ID, "username").send_keys("wronguser")
+    driver.find_element(By.ID, "password").send_keys("wrongpassword")
+    driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+    message = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, ".flash.error"))
+    )
+    assert "Your username is invalid!" in message.text
